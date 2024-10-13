64537c827dfd3466cac78caabb7b34264f2aef71
Gichie <gichie@mail.ru>
2024-10-13 15:37:23 +0300

    test

--- project/Simulation/Map.py
+++ project/Simulation/Map.py
@@ -1,12 +1,28 @@
 from project.Simulation.CreatingObjects import CreatingObjects
+
 class Map:
     '''Карта, содержит в себе коллекцию в виде хеш-таблицы(словаря) для хранения существ(значение) и их расположения(ключ).'''
     def __init__(self, width, height):
         self.map = {}
         self.width = width
         self.height = height
-        for i in range(height):
-            for j in range(width):
+
+    def create_map(self):
+        for i in range(self.height):
+            for j in range(self.width):
                 self.map[(i, j)] = '(...)'
-        for obj in CreatingObjects.creating_objects:
-            self.map[(obj.x, obj.y)] = obj.name
+
+
+    def display_map(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print(self.map[(i, j)], end='')
+            print()
+
+
+
+
+if __name__ == '__main__':
+    map = Map(5, 5)
+    map.create_map()
+    map.display_map()
