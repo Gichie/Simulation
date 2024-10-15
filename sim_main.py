334dcbc856005eaaa5de8409bc3fe94f807fb4b0
Gichie <gichie@mail.ru>
2024-10-15 20:28:29 +0300

    Создание файла, запускающего программу

---
+++ sim_main.py
@@ -0,0 +1,18 @@
+from project.Simulation.Actions import Actions
+from project.Simulation.Map import Map
+from project.setting import Setting
+
+
+class Simulation:
+    Actions.creature()
+    map = Map(Setting().width, Setting().height)
+    map.create_map()
+    map.display_map()
+
+
+
+if __name__ == '__main__':
+    Simulation()
+
+
+
