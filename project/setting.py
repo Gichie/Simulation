dc87faf86211fd1f9061044c32b24d72e9653abc
Gichie <gichie@mail.ru>
2024-10-15 17:55:43 +0300

    Создание файла с настройками приложения

---
+++ project/setting.py
@@ -0,0 +1,12 @@
+class Setting:
+    '''Настройка симуляции'''
+    width = 3
+    height = 3
+    count_grass = 0
+    count_rock = 0
+    count_tree = 0
+    count_herbivore = 1
+    count_predator = 0
+    speed = 1
+    hp = 10
+    pred_strength = 5
